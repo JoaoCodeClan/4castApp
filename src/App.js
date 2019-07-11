47a607b9f5d6e0744a0c26c961b7e2d68417ce7d
joao Nequinha <jcnequinha@gmail.com>
2019-07-11 01:11:23 +0100

    added some error handing on weather fetch function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import SearchBox from"./components/SearchBox";
+import Error from"./components/Error";
 import './App.css';
 
 
 const weatherApi_key="d1169d1918f632fcc4c5ca02b739acf9";
 const googleKey= "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyBUU2lMUxhERixvV19bfd-yJyefPQckUJQ";
 
-
+// TODO make error handling
 class App extends React.Component {
 
 // const geolocation=;
@@ -14,11 +15,11 @@ state={
   weather: null,
   geolocation:null,
   isLoading:false,
-  error:null
+  error:undefined
 }
 
   getWeather=async(e)=>{
-
+    this.setState({error: ""});
     e.preventDefault();
 
     const city=e.target.elements.city.value;
@@ -26,18 +27,48 @@ state={
     const country=e.target.elements.country.value;
 
     const weatherURL=`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${weatherApi_key}&units=Metric`;
+    const weatherURL1=`http://apiopenweathermap.org/data/2.5/weather?q=${city}&appid=${weatherApi_key}&units=Metric`;
 
-
+    if(city&&country){
     fetch(weatherURL).then(res=>{
       if(res.ok){
         return res.json();
+
       }else{
         throw Error("Error retrieving weather data");
+        // this.setState({error: "Error retrieving weather data"});
       }
     }).then(weatherRes=>{
       console.log(weatherRes)
       this.setState({weather:weatherRes})
+      this.setState({error: ""});
+    }).catch(error=>{
+      this.setState({error: error.message})
     })
+  }else if(city && !country){
+    fetch(weatherURL1).then(res=>{
+      if(res.ok){
+        return res.json();
+      }else{
+        throw Error("Error retrieving weather data");
+        // this.setState({error: "Error retrieving weather data"});
+      }
+    }).then(weatherRes=>{
+      console.log(weatherRes)
+      this.setState({weather:weatherRes})
+      this.setState({error: ""});
+    }).catch(error=>{
+      this.setState({error: error.message})
+    })
+  }else{
+    this.setState({error: "Please enter a city"})
+
+
+  }
+
+
+
+
   }
 
   getGeoLoc(){}
@@ -50,6 +81,7 @@ state={
    return(
      <div>
      <SearchBox getWeather={this.getWeather} />
+     <Error error={this.state.error}/>
 
 
      </div>
---
+++ src/components/Error.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Error=props=>(
+<div>
+  <h2>{props.error}</h2>
+  
+</div>
+
+);
+
+export default Error;
