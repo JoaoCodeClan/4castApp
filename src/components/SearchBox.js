2283501ffb8aaf7ac3742780c0a47297fe5fce52
joao Nequinha <jcnequinha@gmail.com>
2019-07-11 21:11:18 +0100

    refactored SearchBox to have updated synthax

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,52 +1,23 @@
 import React from "react";
 
 
-class SearchBox extends React.Component{
-  // constructor(props) {
-  //
-  //   super(props);
-  //   this.state = {
-  //     city:undefined,
-  //     country:undefined,
-  //     geoLoc:null
-  //   }
-  //   this.handleCityInput = this.handleCityInput.bind(this);
-  //   this.handleCountryInput = this.handleCountryInput.bind(this);
-  // }
-  //
-  // handleCityInput(e){
-  //
-  //   console.log(e.target.value);
-  //   this.setState({city: e.target.value});
-  //   console.log(this.state.city);
-  // }
-  // handleCountryInput(e){
-  //   this.setState({country: e.target.elements.country.value});
-  //   console.log(this.state.country)
-  // }
+const SearchBox =(props)=>{
 
-  render(){
-    return(<div>    
-       <form onSubmit={this.props.getWeather} >
+    return(
+       <form onSubmit={props.getWeather} >
          <input type="text" name="city" placeholder="City" />
          <input type="text" name="country" placeholder="Country"/>
          <button id="submit">Search</button>
          <button id="submit" >Around me</button>
        </form>
-       </div>
+
        )
-
-
-
-
-
   }
 
 
 
 
 
-}
 
 
 
